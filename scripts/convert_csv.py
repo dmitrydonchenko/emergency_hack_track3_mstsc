a99107e40fd1c74c56f663ec5d6cf2e23e7ee57e
Alexey Golubev <dr.freecx@gmail.com>
2021-05-28 23:44:25 +0300

    fix

--- scripts/convert_csv.py
+++ scripts/convert_csv.py
@@ -4,20 +4,28 @@ import argparse
 import pandas as pd
 
 
-def convert(folder):
+def convert_file(file, sep):
+    print(f'process {file.name}')
+    df = pd.read_csv(file, sep=sep)
+    output = file.parent / str(file.stem + '.pickle')
+    df.to_pickle(output)
+
+
+def convert(folder, separator):
     for file in folder.glob('*.csv'):
-        print(f'process {file.name}')
-        df = pd.read_csv(file)
-        output = folder.parent / str(file.stem + '.pickle')
-        df.to_pickle(output)
+        convert_file(file, separator)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert csv files to pickle')
-    parser.add_argument('-f', dest='folder', required=True, type=Path, help='folder with csv files')
+    parser.add_argument('-i', dest='input', required=True, type=Path, help='file/folder with csv')
+    parser.add_argument('-s', dest='separator', required=False, default=',', type=str,\
+        help="separator symbol (default is ',')")
 
     args = parser.parse_args()
-    if args.folder.is_dir()
-        convert(args.folder)
+    if args.input.is_dir():
+        convert(args.input, args.separator)
+    elif args.input.is_file():
+        convert_file(args.input, args.separator)
     else:
-        print(f"{args.folder} doesn't seem like folder!")
+        print(f"{args.input} doesn't seem like file or folder!")
